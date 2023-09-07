48f122055b57c41dbc983f9f862a9856aed5f4bd
LizNgoNgoc <liza.ngo@mail.ru>
2023-09-07 18:15:57 +0300

    add signUp changes and Todo css

--- my-app/src/pages/Registration/Registration.jsx
+++ my-app/src/pages/Registration/Registration.jsx
@@ -1,16 +1,35 @@
 import styles from './registration.module.css';
+import { useState } from 'react';
 
 function Regstration() {
-    return <form className={styles.registration}>
+
+    const[inputs, setInputs] = useState({
+        name : '',
+        email : '',
+        password : '',
+        confirm : ''
+    })
+
+    function handleChange({target}) {
+        const {name, value} = target
+        setInputs({...inputs, [name] : value})
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(inputs)
+    }
+
+    return <form className={styles.registration} onSubmit={handleSubmit}>
         <div className={styles.container}>
             <h3 className={styles.header}>Welcome Dolphin!</h3>
             <p className={styles.txt}>Let’s help you meet up your tasks</p>
         </div>
         <div className={styles.inp_container}>
-            <input type="text" className={styles.inp} placeholder='Enter your full name'/>
-            <input type="text" className={styles.inp} placeholder='Enter your email'/>
-            <input type="text" className={styles.inp} placeholder='Enter password' />
-            <input type="text" className={styles.inp} placeholder='Confirm Password' />
+            <input type="text" className={styles.inp} name='name' onChange={handleChange} placeholder='Enter your full name'/>
+            <input type="text" className={styles.inp} name='email' onChange={handleChange} placeholder='Enter your email'/>
+            <input type="text" className={styles.inp} name='password' onChange={handleChange} placeholder='Enter password' />
+            <input type="text" className={styles.inp} name='confirm' onChange={handleChange} placeholder='Confirm Password' />
         </div>
         <div className={styles.reg}>
             <button className={styles.btn}>Registration</button>
