1b17261204577bcf78691547b6019579af98e8c8
LizNgoNgoc <liza.ngo@mail.ru>
2023-10-10 19:33:06 +0300

    add form-todos api

--- my-app/src/components/Form/Form.jsx
+++ my-app/src/components/Form/Form.jsx
@@ -1,17 +1,34 @@
 import styles from './form.module.css'
+import { useEffect, useState } from 'react'
+import api from '../../service/api'
 
 
-function Form() {
 
+
+function Form({setToggle}) {
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        api.apiTodos()
+            .then(todos => {
+                setTodos(todos)})
+    }, [])
+    
+    
+    
     return <div className={styles.container}> 
         <form>
-        <input type="text" className={styles.inp}/>
-        <input type="text" className={styles.inp}/>
-        <input type="text" className={styles.inp}/>
-        <input type="text" className={styles.inp}/>
-        <input type="text" className={styles.inp}/>
+            <button className={styles.btn_task} onClick={() => {setToggle(true)}}></button>
+            {todos.map(todo => {
+            return <div><input type="text" placeholder="Header" className={styles.inp}/>
+            <input type="time" placeholder="Time" className={styles.inp}/>
+            <input type="date" placeholder="Day" className={styles.inp}/>
+            <input type="text" placeholder="Description" className={styles.inp}/>
+            </div>
+        })}
         </form>
     </div>
+   
 }
 
 export default Form;
