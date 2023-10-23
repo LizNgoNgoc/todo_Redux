8b051ffaa0c7b54f26e822ea8531db0e845cbc50
LizNgoNgoc <liza.ngo@mail.ru>
2023-10-23 17:49:51 +0300

    add changes Logout component

---
+++ my-app/src/components/Logout/Logout.jsx
@@ -0,0 +1,37 @@
+import styles from './logout.module.css';
+import api from '../../service/api';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { addUser, userLogout } from '../../redux/slices/User';
+
+
+function Logout() {
+    const user = useSelector(state => state.userSlice)
+    const Navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const logout = (e) => {
+        api.apiLogout()
+            .then((responce) => {
+                if(responce.ok){
+                    dispatch(userLogout())
+                    Navigate('/signIn')
+                }
+            })
+    }
+
+    useEffect(()=>{
+        api.apiForm()
+        .then((data)=>{
+            dispatch(addUser(data))
+            console.log(user)
+        })
+    },[])
+
+    return <div className={styles.logout_cont}>
+                <button className={styles.logout} onClick={logout}>Logout</button>
+            </div>
+}
+
+export default Logout
--- my-app/src/pages/Dashboard/Dashboard.jsx
+++ my-app/src/pages/Dashboard/Dashboard.jsx
@@ -6,35 +6,15 @@ import Form from '../../components/Form/Form';
 import api from '../../service/api';
 import { addUser, userLogout } from '../../redux/slices/User';
 import { useNavigate } from 'react-router-dom';
+import Logout from '../../components/Logout/Logout';
 
 function Dashboard () {
     
     const [toggle, setToggle] = useState(false)
-    const Navigate = useNavigate()
     const user = useSelector(state => state.userSlice)
-    const dispatch = useDispatch()
-    const logout = (e) => {
-        api.apiLogout()
-            .then((responce) => {
-                if(responce.ok){
-                    dispatch(userLogout())
-                    Navigate('/signIn')
-                }
-            })
-    }
     
-    useEffect(()=>{
-        api.apiForm()
-        .then((data)=>{
-            dispatch(addUser(data))
-            console.log(user)
-        })
-    },[])
-
     return <section className={styles.dashboard}>
-        <div className={styles.logout_cont}>
-            <button className={styles.logout} onClick={logout}>Logout</button>
-        </div>
+        <Logout />
         <div className={styles.header_container}>
             <img src="./images/Ellipse 11.png" className={styles.img} alt="img" />
             <h3 className={styles.header}>{`Welcome, ${user.name}!`}</h3>
